898f044e0f9e1011eec7e2bad4a3a4160f42df2a
Muhammad Junaid <648mjunaid@gmail.com>
2023-04-12 00:22:01 +0500

    done with lint, .env file and other change requests

---
+++ backend/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+  rules: {
+    // Add any additional rules here
+  },
+};
--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const recipes = require('./routes/recipes')
+const recipes = require("./routes/recipes");
+require("dotenv").config();
 
-const DATABASE =
-  "mongodb+srv://Junaid648:Juni%408808@recipe-app.5rhnbbe.mongodb.net/?retryWrites=true&w=majority";
 const port = process.env.PORT || 8000;
-
-mongoose.connect(DATABASE)
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error(err.message));
-
 const app = express();
 app.use(express.json());
-app.use('/recipes',recipes);
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.use("/recipes", recipes);
+
+async function startServer() {
+  try {
+    await mongoose.connect(process.env.DATABASE, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log("Connected to MongoDB...");
+    app.listen(port, () => console.log(`Listening on port ${port}...`));
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+startServer();
