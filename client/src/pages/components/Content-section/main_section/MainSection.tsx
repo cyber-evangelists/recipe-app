388d8f1808a9961ceb6f2deb340284a9880f6ac6
Muhammad Junaid <648mjunaid@gmail.com>
2023-05-03 13:02:49 +0500

    done with team-lead suggestions

--- client/src/pages/components/Content-section/main_section/MainSection.tsx
+++
@@ -1,9 +0,0 @@
-import Banner_section from "./Advertisement Section/Advertisement_section";
-import styles from "./main_section.module.css";
-export default function MainSection() {
-  return (
-    <div className={styles.main_section}>
-      <Banner_section/>
-    </div>
-  );
-}
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import MainPage from "./components/MainPage";
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Recipe App</title>
+      </Head>
+      <MainPage />
+      
+       
+    </>
+  );
+}
